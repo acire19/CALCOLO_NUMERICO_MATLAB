b3c4ce0a66c21eafa8ca9d582134f070b046d070
acire19 <acire19@virgilio.it>
2013-10-22 21:30:05 +0200

    first exerc.

---
+++ ESERCIZIO_3_F1.m
@@ -0,0 +1,19 @@
+%------ ESERCIZIO n.3 - FOGLIO 1 ------%
+%------ ESERCIZIO n.3 - FOGLIO 1 ------%
+%------ ESERCIZIO n.3 - FOGLIO 1 ------%
+
+n=10;               % dimensione della matrice.
+x=5*ones(n,1);      % vettore a n componenti uguali a 1.
+AD=diag(x);         % matrice diagonale dove sulla diag ho n elementi uguali a 5.
+y=3*ones(n-1,1);    % sulla codiagonale ho n-1 elementi... codiag superiore
+ACS=diag(y,1);
+z=-ones(n-1, 1);    % codiagon inferiore.
+ACI=diag(z, -1);
+
+% sommo le tre matrici... 
+A=AD+ACS+ACI;
+
+% modifico alcuni elementi...
+A([5 8],:)=2           % selezione righe 5,8 e tutte le colonne 
+                       % e pongo qst elementi uguali a 2.
+                       
